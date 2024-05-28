3d4aa9bd90c435b7cf9d9f1f605be5c3f108db5b
rajeev262-cyber <rajeevyadav5758@gmail.com>
2024-05-28 22:29:25 +0530

    image scrapping

--- app.py
+++ app.py
@@ -35,8 +35,7 @@ def index():
                     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"}
 
                             # fetch the search results page
-                    response = requests.get(f"https://www.google.com/search?q={query}&sxsrf=AJOqlzUuff1RXi2mm8I_OqOwT9VjfIDL7w:1676996143273&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiq-qK7gaf9AhXUgVYBHYReAfYQ_AUoA3oECAEQBQ&biw=1920&bih=937&dpr=1#imgrc=1th7VhSesfMJ4M")
-
+                    response = requests.get(f"https://www.google.co.in/search?sca_esv=7dad08e1366017e7&sca_upv=1&q={query}&uds=ADvngMjwX4dvqjExbCg9zqvMuh5VVF0FDbCEM60QOBAptkt_-T8p-ulcf1r_DNSu3B1qeE3u_maggxt-IID1oBGMtRGXQjYEyyXut-1zbfhfjs4VQDpmFpBLgBevNFHHkWq5xJIXu3IYYjimV4EdloZccE4JnrQSs9KSj7__2NwLNJNbva5I4PfNrOuyU1jyTBfZxA3Jwj_nMtY8BjPrEOfW3WRCamy-ZE_RVcgmg5eHmX09jb36fCXFinoJhV32UxyPiWREqNc5-ZdefuVRD8gMLZLVpJsZ5-O1TL1bMs4rIVRXpkF1MxgDvj3ZjfNNAR4k6hi52j2a&udm=2&prmd=ivnsmbtz&sa=X&ved=2ahUKEwik_aLw27CGAxWMyjgGHU4DDVQQtKgLegQIEhAB&biw=1488&bih=742&dpr=1.25")
 
                             # parse the HTML using BeautifulSoup
                     soup = BeautifulSoup(response.content, "html.parser")
@@ -58,10 +57,10 @@ def index():
                                 img_data.append(mydict)
                                 with open(os.path.join(save_directory, f"{query}_{image_tags.index(image_tag)}.jpg"), "wb") as f:
                                     f.write(image_data)
-                    client = pymongo.MongoClient("mongodb+srv://snshrivas:Snshrivas@cluster0.ln0bt5m.mongodb.net/?retryWrites=true&w=majority")
-                    db = client['image_scrap']
-                    review_col = db['image_scrap_data']
-                    review_col.insert_many(img_data)          
+                #     client = pymongo.MongoClient("mongodb+srv://snshrivas:Snshrivas@cluster0.ln0bt5m.mongodb.net/?retryWrites=true&w=majority")
+                #     db = client['image_scrap']
+                #     review_col = db['image_scrap_data']
+                #     review_col.insert_many(img_data)          
 
                     return "image laoded"
                 except Exception as e:
